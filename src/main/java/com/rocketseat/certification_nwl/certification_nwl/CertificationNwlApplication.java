0bf517cda61afb7d564218b3e88ff7c2f2770549
luizamachado <luizamachado@ciandt.com>
2024-02-07 16:45:45 -0300

    Primeiro commit

---
+++ src/main/java/com/rocketseat/certification_nwl/certification_nwl/CertificationNwlApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.certification_nwl.certification_nwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationNwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationNwlApplication.class, args);
+	}
+
+}
